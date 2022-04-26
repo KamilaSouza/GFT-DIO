7cd4029356838c7e37266425ea2c13f749bc1db7
Kamila <kamisouzas@gmail.com>
2022-04-26 11:16:37 -0300

    Challenge3 - set

--- Challenge/Desafio3ADC/src/Main.java
+++ Challenge/Desafio3ADC/src/Main.java
@@ -6,12 +6,15 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.printf("Insert the number of slots: ");
         int numTotal = scanner.nextInt();
+        System.out.printf("Insert the number of packs you bought: ");
         int numFigCompradas = scanner.nextInt();
+        System.out.printf("Insert the code of cards: ");
         Set<Integer> setFig = new HashSet<>();
         for (int i = 0; i < numFigCompradas; i++) {
             setFig.add(scanner.nextInt());
         }
-        System.out.println(numTotal - setFig.size());
+        System.out.println("Number of cards you need to complete the album: " + (numTotal - setFig.size()));
     }
 }
